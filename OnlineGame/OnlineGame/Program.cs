520e4f8bcc5b33f480aaf0235da3fd2a0788a286
Steve-Htet <skyking.mm.com@gmail.com>
2023-08-24 12:00:30 +0630

    online game calculate

---
+++ OnlineGame/OnlineGame/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           int games = Convert.ToInt32(Console.ReadLine());
+           int wins = Convert.ToInt32(Console.ReadLine());
+
+            Player player1 = new Player();
+            player1.games = games;
+            player1.wins = wins;
+
+            player1.GetWinRate();
+            Console.ReadKey();
+            }
+        class Player{
+            public int games;
+            public int wins;
+            private int winrate;
+
+            public int Winrate{
+                get {return winrate;}
+                set {winrate = value;}
+                }
+
+            public void GetWinRate(){
+               winrate = (wins*100/games);
+                Console.WriteLine(winrate);
+                }
+            }
+     }
+   
+}
+
